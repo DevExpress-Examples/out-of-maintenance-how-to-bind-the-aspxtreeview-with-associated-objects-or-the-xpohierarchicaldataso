f15f06c93e28c2c050e4e6e298283ff25202a5c7
DevExpressExampleBot <DevExpressExampleBot@users.noreply.github.com>
2018-04-23 19:21:59 +0300

    initial commit

---
+++ CS/WebSite/App_Code/XpoHierarchicalDataSource.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Web;
+using System.Web.UI;
+using DevExpress.Xpo;
+
+/// <summary>
+/// Summary description for XpoHierarchicalDataSource
+/// </summary>
+public class XpoHierarchicalDataSource<T> : IHierarchicalDataSource {
+    public Session Session { get; set; }
+
+    public String ParentProperty { get; set; }
+    public String ChildrenProperty { get; set; }
+
+    XpoHierarchicalDataSourceView<T> view;
+
+    public XpoHierarchicalDataSource() { }
+
+    public event EventHandler DataSourceChanged;
+
+    public HierarchicalDataSourceView GetHierarchicalView(string viewPath) {
+        if (view == null)
+            view = new XpoHierarchicalDataSourceView<T>(Session, viewPath, ParentProperty, ChildrenProperty);
+
+        return view;
+    }
+}
